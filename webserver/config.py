e302448278422a16e798a0bbe4564b98d77f1475
lucasbutzke <lucasbutzke28@gmail.com>
2025-10-16 14:46:01 +0100

    [RTOP-78] Fix restapi database and env directory location for docker volumes

--- webserver/config.py
+++ webserver/config.py
@@ -6,8 +6,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # Always resolve .env relative to the repo root to guarantee it is found
-ENV_PATH = Path(__file__).resolve().parent.parent / ".env"
-DB_PATH = Path(__file__).resolve().parent.parent / "restapi.db"
+ENV_PATH = Path(__file__).resolve().parent.parent / "webserver/.env"
+DB_PATH = Path(__file__).resolve().parent.parent / "webserver/restapi.db"
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # logger = logging.getLogger("logger")
@@ -35,18 +35,18 @@ def generate_env_file():
         f.write(f"PEPPER={pepper}\n")
 
     os.chmod(ENV_PATH, 0o600)
-    logger.info(f".env file created at {ENV_PATH}")
+    print(f".env file created at {ENV_PATH}")
 
     # Ensure the database file exists and is writable
     # Deletion is required because new secrets will change the database saved hashes
     if os.path.exists(DB_PATH):
         os.remove(DB_PATH)
-        logger.info(f"Deleted existing database file: {DB_PATH}")
+        print(f"Deleted existing database file: {DB_PATH}")
 
 
 # Load .env file
 if not os.path.isfile(ENV_PATH):
-    logger.warning(".env file not found, creating one...")
+    print(".env file not found, creating one...")
     generate_env_file()
 
 load_dotenv(dotenv_path=ENV_PATH, override=False)
@@ -58,7 +58,7 @@ try:
         if not val or not is_valid_env(var, val):
             raise RuntimeError(f"Environment variable '{var}' is invalid or missing")
 except RuntimeError as e:
-    logger.error(f"{e}")
+    print(f"{e}")
     # Need to regenerate .env file and remove the database as well
     response = (
         input(
@@ -68,11 +68,11 @@ except RuntimeError as e:
         .lower()
     )
     if response == "y":
-        logger.info("Regenerating .env with new valid values...")
+        print("Regenerating .env with new valid values...")
         generate_env_file()
         load_dotenv(ENV_PATH)
     else:
-        logger.error("Exiting due to invalid environment configuration.")
+        print("Exiting due to invalid environment configuration.")
         exit(1)
 
 
