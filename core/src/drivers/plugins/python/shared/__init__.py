90e496ac3de01cf7efd133ee8d3f88d3c559c45f
Marcone Tenorio <marconetsf@gmail.com>
2025-10-16 10:26:56 +0200

    adjusting __ini__ from shared module

--- core/src/drivers/plugins/python/shared/__init__.py
+++ core/src/drivers/plugins/python/shared/__init__.py
@@ -3,19 +3,16 @@ OpenPLC Python Plugin Configuration Package
 """
 
 from .plugin_config_decode.plugin_config_contact import PluginConfigContract, PluginConfigError
-from .plugin_config_decode.modbus_master_config_model import ModbusIoPointConfig, ModbusTcpConfig
-from .plugin_config_decode.plugin_config_decoder import PluginInstanceConfig, PluginConfigDecoder
+from .plugin_config_decode.modbus_master_config_model import ModbusIoPointConfig, ModbusMasterConfig
 
 __all__ = [
     # abstract contract for each protocol config model
     'PluginConfigContract',
     # top level config instance
     'PluginConfigError', 
-    'PluginConfigDecoder',
-    'PluginInstanceConfig',
     # concrete protocol config models
     'ModbusIoPointConfig',
-    'ModbusTcpConfig',
+    'ModbusMasterConfig',
     # concrete protocol config models
     # 'EthercatConfig',
     # 'EthercatIoPointConfig',
