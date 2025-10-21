a3d15e557fe88ae6bad3e210ec22d1e4a5af4ac3
lucasbutzke <lucasbutzke28@gmail.com>
2025-10-21 01:42:20 +0100

    [RTOP-89] Pytest docker setup

--- tests/pytest/conftest.py
+++ tests/pytest/conftest.py
@@ -1,6 +1,9 @@
 # tests/conftest.py
 import pytest
 from webserver.logger import get_logger
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 logger, buffer = get_logger("test_logger", use_buffer=True)
 
